dd330fb58819f38fa6b7285baaaed15195445020
sentientswitch <sentientswitch@gmail.com>
2009-05-14 01:20:38 +0100

    Added GetStat,NCInit,NCExit functions.
    GetStat reads a given stats file.
    NC.. functions contain ncurses initialisation/exit functions.

--- sentop.c
+++ sentop.c
@@ -6,14 +6,59 @@
 
 
 #include <ncurses.h>
+#include <stdio.h>
+
+//Global Vars.
+char STATS_PATH[100] = "/sys/devices/pci0000:00/0000:00:1c.5/0000:04:00.0/net/eth0/statistics/";
+
+//Functions.
+unsigned int GetStat(char* statsFile);
+
+
+void NCInit() {
+  initscr();
+  keypad(stdscr, TRUE);
+}
+
+void NCExit() {
+  endwin();
+}
+
+//
+//  GetStat
+//
+//  Takes name of statistics file.
+//
+//  Returns number from statistics file as unsigned int.
+//
+unsigned int GetStat(char statsFile[]) {
+  unsigned int retVal;
+  char filePath[200];
+  FILE *filePtr;
+
+  //Get full file path
+  sprintf(filePath, "%s%s", STATS_PATH, statsFile);
+
+  //Open, read, close.
+  filePtr = fopen(filePath, "r");
+  fscanf(filePtr, "%u", &retVal);
+  fclose(filePtr);
+
+  return retVal;
+}
 
 int main (int argc, char* argv[]) {
+  unsigned int txBytes;
+  char sTxBytes[20]; //String to hold txBytes.
+
+  txBytes = GetStat("tx_bytes");
+  sprintf(sTxBytes, "%u", txBytes);
 
   //Init ncurses.
-  initscr();
+  NCInit();
 
   //Do stuff.
-  printw("Hello World !!!");
+  printw(sTxBytes);
 
   //Print it on the screen.
   refresh();
@@ -22,7 +67,7 @@ int main (int argc, char* argv[]) {
   getch();
 
   //End ncurses.
-  endwin();
+  NCExit();
 
   return 0;
 }

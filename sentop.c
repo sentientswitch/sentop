cf7fb5d156ad997f7d6d63c46f77282a03fe2dd6
sentientswitch <sentientswitch@gmail.com>
2009-05-14 13:04:00 +0100

    Added snapshot structure + function.

--- sentop.c
+++ sentop.c
@@ -12,10 +12,26 @@
 char STATS_PATH[100] = "/sys/devices/pci0000:00/0000:00:1c.5/0000:04:00.0/net/eth0/statistics/"; //Interface statistics path.
 static WINDOW* ROOT_WIN; //Pointer to root window.
 
+//Structs.
+struct snap_all {
+  unsigned int rx_bytes,           tx_bytes;
+  unsigned int rx_packets,         tx_packets;
+  unsigned int rx_errors,          tx_errors;
+  unsigned int rx_dropped,         tx_dropped;
+  unsigned int rx_fifo_errors,     tx_fifo_errors;
+  unsigned int rx_compressed,      tx_compressed;
+
+  unsigned int rx_crc_errors,      rx_frame_errors,      rx_length_errors,  rx_missed_errors,  rx_over_errors;
+  unsigned int tx_carrier_errors,  tx_heartbeat_errors,  tx_window_errors,  tx_aborted_errors;
+
+  unsigned int collisions,         multicast;
+};
+
 //Functions.
 void NCInit();
 void NCExit();
 unsigned int GetStat(char* statsFile);
+struct snap_all GetSnapAll();
 
 //--------------------------------------------------------------//
 //  NCInit                                                      //
@@ -61,6 +77,39 @@ unsigned int GetStat(char statsFile[]) {
   return retVal;
 }
 
+struct snap_all GetSnapAll() {
+  struct snap_all retSnap;
+
+  //Get shared vals.
+  //                   rx                                                    tx
+  retSnap.rx_bytes             = GetStat("rx_bytes");           retSnap.tx_bytes        = GetStat("tx_bytes");
+  retSnap.rx_packets           = GetStat("rx_packets");         retSnap.tx_packets      = GetStat("tx_packets");
+  retSnap.rx_errors            = GetStat("rx_errors");          retSnap.tx_errors       = GetStat("tx_errors");
+  retSnap.rx_dropped           = GetStat("rx_dropped");         retSnap.tx_dropped      = GetStat("tx_dropped");
+  retSnap.rx_fifo_errors       = GetStat("rx_fifo_errors");     retSnap.tx_fifo_errors  = GetStat("tx_fifo_errors");
+  retSnap.rx_compressed        = GetStat("rx_compressed");      retSnap.tx_compressed   = GetStat("tx_compressed");
+
+  //Get rx only vals.
+  retSnap.rx_crc_errors        = GetStat("rx_crc_errors");
+  retSnap.rx_frame_errors      = GetStat("rx_frame_errors");
+  retSnap.rx_length_errors     = GetStat("rx_length_errors");
+  retSnap.rx_missed_errors     = GetStat("rx_missed_errors");
+  retSnap.rx_over_errors       = GetStat("rx_over_errors");
+
+  //Get tx only vals.
+  retSnap.tx_carrier_errors    = GetStat("tx_carrier_errors");
+  retSnap.tx_heartbeat_errors  = GetStat("tx_heartbeat_errors");
+  retSnap.tx_window_errors     = GetStat("tx_window_errors");
+  retSnap.tx_aborted_errors    = GetStat("tx_aborted_errors");
+
+  //Get other vals.
+  retSnap.collisions           = GetStat("collisions");
+  retSnap.multicast            = GetStat("multicast");
+
+  return retSnap;
+}
+
+
 //--------------------------------------------------------------//
 //  main                                                        //
 //--------------------------------------------------------------//
@@ -78,6 +127,8 @@ int main (int argc, char* argv[]) {
 
   //Do stuff.
   box(ROOT_WIN, ACS_VLINE, ACS_HLINE);
+
+  mvprintw(1, 1, "tx_bytes: ");
   printw(sTxBytes);
 
   //Print it on the screen.
